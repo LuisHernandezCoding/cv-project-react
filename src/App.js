def34258a2cecaefce12de536ba462b40b781815
LuisHernandezCoding <luishernandezcoding@gmail.com>
2023-01-20 00:46:45 -0600

    Added Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Sidebar />
+      <main className="content" />
+    </div>
   );
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h1 className="sidebar__title slideInFromLeft">
+        Made with
+        <span role="img" aria-label="love"> ❤️ </span>
+        by
+        <div>
+          <a href="http://github.com/LuisHernandezCoding">Luis Hernandez</a>
+        </div>
+      </h1>
+      <h1 className="sidebar__title slideInFromLeft delay-half">
+        Made with
+        <span role="img" aria-label="love"> ❤️ </span>
+        by
+        <div>
+          <a href="http://github.com/LuisHernandezCoding">Luis Hernandez</a>
+        </div>
+      </h1>
+      <h1 className="sidebar__title slideInFromLeft delay-1">
+        Made with
+        <span role="img" aria-label="love"> ❤️ </span>
+        by
+        <div>
+          <a href="http://github.com/LuisHernandezCoding">Luis Hernandez</a>
+        </div>
+      </h1>
+    </aside>
+  );
+}
