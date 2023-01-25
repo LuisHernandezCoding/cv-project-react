6fa4d1c91dd7d3b878cd4611bcbb10bed621cc81
LuisHernandezCoding <luishernandezcoding@gmail.com>
2023-01-25 15:16:33 -0600

    Rewrite EmploymentFormInfo to functional component

--- src/components/form/EmploymentFormInfo.js
+++ src/components/form/EmploymentFormInfo.js
@@ -1,40 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import SimpleInput from '../helpers/SimpleInput';
 import SumableInput from '../helpers/SumableInput';
 import IconButton from '../helpers/IconButton';
 import DateInput from '../helpers/DateInput';
 
-export default class EmploymentFormInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      employments: {},
-    };
-  }
+const EmploymentFormInfo = () => {
+  const [employments, setEmployments] = useState({});
 
-  componentDidMount() {
-    this.loadState();
-  }
-
-  componentDidUpdate() {
-    this.saveState();
-  }
-
-  saveState = () => {
-    const { employments } = this.state;
-    localStorage.setItem('employments', JSON.stringify(employments));
-  }
-
-  loadState = () => {
-    const employments = JSON.parse(localStorage.getItem('employments'));
-    if (employments) {
-      this.setState({ employments });
+  useEffect(() => {
+    const employmentsFromStorage = localStorage.getItem('employments');
+    if (employmentsFromStorage) {
+      const parsedEmployments = JSON.parse(employmentsFromStorage);
+      if (JSON.stringify(parsedEmployments) !== JSON.stringify(employments)) {
+        setEmployments(parsedEmployments);
+      }
+    } else {
+      setEmployments({});
     }
-  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  addEmployment = () => {
-    const { employments } = this.state;
+  useEffect(() => {
+    localStorage.setItem('employments', JSON.stringify(employments));
+  }, [employments]);
+
+  const addEmployment = () => {
     const newEmployment = {
       company: 'Company',
       position: 'Position',
@@ -42,152 +33,141 @@ export default class EmploymentFormInfo extends React.Component {
       endDate: 'End Date',
       descriptions: {},
     };
-    this.setState(
-      {
-        employments: {
-          ...employments,
-          [Object.keys(employments).length]: newEmployment,
-        },
-      },
-    );
-  }
+    setEmployments({ ...employments, [Object.keys(employments).length]: newEmployment });
+  };
 
-  removeEmployment = (key) => {
-    const { employments } = this.state;
-    delete employments[key];
-    this.setState({ employments });
-  }
+  const removeEmployment = (key) => {
+    const newEmployments = { ...employments };
+    delete newEmployments[key];
+    setEmployments(newEmployments);
+  };
 
-  editEmployment = (key, name, value) => {
-    const { employments } = this.state;
-    employments[key][name] = value;
-    this.setState({ employments });
-  }
+  const editEmployment = (key, name, value) => {
+    const newEmployments = { ...employments };
+    newEmployments[key][name] = value;
+    setEmployments(newEmployments);
+  };
 
-  addDescription = (key, description) => {
-    const { employments } = this.state;
-    const { descriptions } = employments[key];
-    descriptions[description] = description;
-    this.setState({ employments });
+  const addDescription = (key, description) => {
+    const newEmployments = { ...employments };
+    newEmployments[key].descriptions[description] = description;
+    setEmployments(newEmployments);
     setTimeout(() => {
       const newDescription = document.getElementById(description);
       newDescription.focus();
     }, 0);
-  }
+  };
 
-  removeDescription = (key, id) => {
-    const { employments } = this.state;
-    const { descriptions } = employments[key];
-    delete descriptions[id];
-    this.setState({ employments });
-  }
+  const removeDescription = (key, id) => {
+    const newEmployments = { ...employments };
+    delete newEmployments[key].descriptions[id];
+    setEmployments(newEmployments);
+  };
 
-  editDescription = (key, id, value) => {
-    const { employments } = this.state;
-    const { descriptions } = employments[key];
-    descriptions[id] = value;
+  const editDescription = (key, id, value) => {
+    const newEmployments = { ...employments };
+    newEmployments[key].descriptions[id] = value;
     if (id !== value) {
-      descriptions[value] = descriptions[id];
-      delete descriptions[id];
+      newEmployments[key].descriptions[value] = newEmployments[key].descriptions[id];
+      delete newEmployments[key].descriptions[id];
     }
     if (id === 'New Description' || value === '') {
-      delete descriptions[id];
+      delete newEmployments[key].descriptions[id];
     }
-    this.setState({ employments });
-  }
+    setEmployments(newEmployments);
+  };
 
-  render() {
-    const { employments } = this.state;
-    return (
-      <div>
-        <span className="profileTitle">
-          <span className="icon">
-            <i className="fas fa-book" />
-          </span>
-          &nbsp;
-          Employment History
-          <IconButton
-            className="titleButton"
-            onClick={this.addEmployment}
-            icon="fas fa-plus"
-          />
+  return (
+    <div>
+      <span className="profileTitle">
+        <span className="icon">
+          <i className="fas fa-book" />
         </span>
-        {Object.keys(employments).map((key) => (
-          <div key={key} className="p-1">
-            <hr />
-            <div className="flex mobile">
-              <div className="is-half flex">
-                <SimpleInput
-                  size="big"
-                  defaultValue="Position"
-                  icon=""
-                  callback={(value) => this.editEmployment(key, 'position', value)}
-                  actualValue={employments[key].position}
-                />
-              </div>
-              <div className="is-half flex">
-                <span className="hidden-mobile">At: </span>
-                <SimpleInput
-                  size="big"
-                  defaultValue="Company"
-                  icon=""
-                  callback={(value) => this.editEmployment(key, 'company', value)}
-                  actualValue={employments[key].company}
-                />
-              </div>
-              <div>
-                <IconButton
-                  className="button employment close"
-                  onClick={() => this.removeEmployment(key)}
-                  icon="fas fa-times"
-                />
-              </div>
+        &nbsp;
+        Employment History
+        <IconButton
+          className="titleButton"
+          onClick={addEmployment}
+          icon="fas fa-plus"
+        />
+      </span>
+      {Object.keys(employments).map((key) => (
+        <div key={key} className="p-1">
+          <hr />
+          <div className="flex mobile">
+            <div className="is-half flex">
+              <SimpleInput
+                size="big"
+                defaultValue="Position"
+                icon=""
+                callback={(value) => editEmployment(key, 'position', value)}
+                actualValue={employments[key].position}
+              />
             </div>
-            <hr />
-            <div className="flex mobile mt-1">
-              <div className="is-half flex p-1">
-                <DateInput
-                  actualValue={employments[key].startDate}
-                  callback={(value) => this.editEmployment(key, 'startDate', value)}
-                  defaultValue="Start Date"
-                  prefix="From: "
-                  dateRange="past"
-                />
-                <DateInput
-                  actualValue={employments[key].endDate}
-                  callback={(value) => this.editEmployment(key, 'endDate', value)}
-                  defaultValue="End Date"
-                  prefix="To: "
-                  dateRange="past"
-                />
-              </div>
-              <div className="is-half flex p-1">
-                <span className="formIcon big"><i className="fas fa-pencil" /></span>
-                <div className="descriptionTitle">
-                  Job Responsibilities
-                  <IconButton
-                    className="descriptionButton"
-                    onClick={() => this.addDescription(key, 'New Description')}
-                    icon="fas fa-plus"
-                  />
-                </div>
-              </div>
+            <div className="is-half flex">
+              <span className="hidden-mobile">At: </span>
+              <SimpleInput
+                size="big"
+                defaultValue="Company"
+                icon=""
+                callback={(value) => editEmployment(key, 'company', value)}
+                actualValue={employments[key].company}
+              />
             </div>
             <div>
-              {Object.keys(employments[key].descriptions).map((id) => (
-                <SumableInput
-                  key={id}
-                  inputId={id}
-                  inputName={employments[key].descriptions[id]}
-                  defaultValue="New Description"
-                  removeInput={(value) => this.removeDescription(key, value)}
-                  editInput={(value) => this.editDescription(key, id, value)}
-                />
-              ))}
+              <IconButton
+                className="button employment close"
+                onClick={() => removeEmployment(key)}
+                icon="fas fa-times"
+              />
             </div>
           </div>
-        ))}
-      </div>
-    );
-  }
-}
+          <hr />
+          <div className="flex mobile mt-1">
+            <div className="is-half flex p-1">
+              <DateInput
+                actualValue={employments[key].startDate}
+                callback={(value) => editEmployment(key, 'startDate', value)}
+                defaultValue="Start Date"
+                prefix="From: "
+                dateRange="past"
+              />
+              <DateInput
+                actualValue={employments[key].endDate}
+                callback={(value) => editEmployment(key, 'endDate', value)}
+                defaultValue="End Date"
+                prefix="To: "
+                dateRange="past"
+              />
+            </div>
+            <div className="is-half flex p-1">
+              <span className="formIcon big"><i className="fas fa-pencil" /></span>
+              <div className="descriptionTitle">
+                Job Responsibilities
+                <IconButton
+                  className="descriptionButton"
+                  onClick={() => addDescription(key, 'New Description')}
+                  icon="fas fa-plus"
+                />
+              </div>
+            </div>
+          </div>
+          <div>
+            {Object.keys(employments[key].descriptions).map((id) => (
+              <SumableInput
+                key={id}
+                inputId={id}
+                inputName={employments[key].descriptions[id]}
+                defaultValue="New Description"
+                removeInput={(value) => removeDescription(key, value)}
+                editInput={(value) => editDescription(key, id, value)}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EmploymentFormInfo;
